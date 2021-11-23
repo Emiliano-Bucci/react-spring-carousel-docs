2ebaf61764202920d99dfb89f97cd26ae16ed3b5
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-11-23 15:42:25 +0100

    feat: init project

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const withLinaria = require("next-linaria");
+const withPlugins = require("next-compose-plugins");
+
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      // issuer section restricts svg as component only to
+      // svgs imported from js / ts files.
+      //
+      // This allows configuring other behavior for
+      // svgs imported from other file types (such as .css)
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
+
+module.exports = withLinaria(nextConfig);
